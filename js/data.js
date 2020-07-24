6a77de6ad011ab9730a7eebd1f1c723a481660e6
LaurynasAnd <67257508+LaurynasAnd@users.noreply.github.com>
2020-07-24 09:25:38 +0300

    Months in words

--- js/data.js
+++ js/data.js
@@ -1,14 +1,14 @@
 const account = [
-    { month: 10, income: 150 },
-    { month: 1, income: 200, expense: 50 },
-    { month: 3, income: 450, expense: 200 },
-    { month: 7, expense: 50 },
-    { month: 2, income: 50 },
-    { month: 5, income: 150, expense: 50 },
-    { month: 9, income: 700, expense: 400 },
-    { month: 4, income: 10, expense: 300 },
-    { month: 6, expense: 50 },
-    { month: 11, income: 150, expense: 30 },
-    { month: 8, income: 450, expense: 120 },
-    { month: 12, expense: 200 }
+    { month: 10, monthWord: Spalis, income: 150 },
+    { month: 1, monthWord: Sausis, income: 200, expense: 50 },
+    { month: 3, monthWord: Kovas, income: 450, expense: 200 },
+    { month: 7, monthWord: Liepa, expense: 50 },
+    { month: 2, monthWord: Vasaris, income: 50 },
+    { month: 5, monthWord: Gegužė, income: 150, expense: 50 },
+    { month: 9, monthWord: Rugsėjis, income: 700, expense: 400 },
+    { month: 4, monthWord: Balandis, income: 10, expense: 300 },
+    { month: 6, monthWord: Birželis, expense: 50 },
+    { month: 11, monthWord: Lapkritis, income: 150, expense: 30 },
+    { month: 8, monthWord: Rugpjūtis, income: 450, expense: 120 },
+    { month: 12, monthWord: Gruodis, expense: 200 }
 ];
