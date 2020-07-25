aba5aceefe9d2f78397bbfef8413572247fddbe3
Mantas  Šalkovskis <mantasssalkovskis@gmail.com>
2020-07-25 22:24:10 +0300

    3.
    3. Užpildyti kintamąjį, kur turi būti surašyti visų mėnesių pavadinimai;

---
+++ js/main.js
@@ -0,0 +1,18 @@
+import  account from './data.js';
+
+console.log(account);
+
+const month = {
+    1:'sausis',
+    2:'vasaris',
+    3:'kovas',
+    4:'balandis',
+    5:'gegužė',
+    6:'birželis',
+    7:'liepa',
+    8:'rugpjūtis',
+    9:'rugsėjis',
+    10:'spalis',
+    11:'lapkritis',
+    12:'gruodis',
+};
